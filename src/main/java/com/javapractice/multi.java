7d6329ba58a7917b27dd933663fb62e21a08cb26
kalyanbhargav1432 <tannirukalyan1432@gmail.com>
2021-09-22 15:59:39 +0530

    Added: multi

---
+++ src/main/java/com/javapractice/multi.java
@@ -0,0 +1,9 @@
+package com.javapractice;
+
+public class multi {
+ public static void main(String[] args) {
+  int a=4,b=5;
+  int c=a*b;
+  System.out.println("multi:"+c);
+}
+}
--- src/main/java/com/javapractice/subtraction.java
+++ src/main/java/com/javapractice/subtraction.java
@@ -2,7 +2,7 @@ package com.javapractice;
 
 public class subtraction {
  public static void main(String[] args) {
-   int a=20,b=14;
+   int a=20, b=14;
    int c=a-b;
    System.out.println("subtraction:"+c);
 }
